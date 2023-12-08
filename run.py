5e965e6cdcce5bf70a6420ab1c88299c68b3e26a
Hakan Ardo <hakan.ardo@spiideo.com>
2023-12-08 13:37:13 +0100

    Clear out some old stuff that was never released

--- run.py
+++ run.py
@@ -12,24 +12,17 @@ import bpy
 
 from blenderset.render import PreviewRenderer, Renderer
 from blenderset.scenarios import (
-    ConstructionRealBack,
-    ForestRoad,
     Nyhamnen,
-    DelfinenSynthBack,
-    DelfinenRealBack,
     RealHighway,
 )
 
 
 def main():
-    # root = Path('renders/nyhamnen')
-    #     root = Path("renders/delfinen_realback")
-    # root = Path("renders/construction_realback")
-    # root = Path("renders/forest_road2")
-    root = Path("renders/real_highway")
+    root = Path('renders/nyhamnen')
+    # root = Path("renders/real_highway")
 
-    # renderer = PreviewRenderer(bpy.context, root)
-    renderer = Renderer(bpy.context, root)
+    renderer = PreviewRenderer(bpy.context, root)
+    # renderer = Renderer(bpy.context, root)
 
     run_start = datetime.datetime.now()
     run_name = os.environ.get(
@@ -40,12 +33,8 @@ def main():
 
     for scene_num in range(1000):
         bpy.ops.wm.open_mainfile(filepath="blank.blend")
-        # gen = Nyhamnen(bpy.context, randint(20, 200), test_set=True)
-        # gen = DelfinenSynthBack(bpy.context, 2)
-        # gen = DelfinenRealBack(bpy.context, randint(10, 30))
-        # gen = ConstructionRealBack(bpy.context, randint(3, 6))
-        # gen = ForestRoad(bpy.context, randint(10, 20))
-        gen = RealHighway(bpy.context, randint(20, 30))
+        gen = Nyhamnen(bpy.context, randint(20, 200), test_set=True)
+        # gen = RealHighway(bpy.context, randint(20, 30))
         gen.create()
         for perm_num in range(10):
             renderer.render(gen, f"{run_name}_{scene_num:03}_{perm_num:03}")
