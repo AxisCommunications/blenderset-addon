48741fcd603657df64db8f6128b47093f621eaea
Hakan Ardo <hakan.ardo@spiideo.com>
2023-12-18 14:45:19 +0100

    rotate mesh vertices instead of bounding boxes

--- blenderset/utils/exr.py
+++ blenderset/utils/exr.py
@@ -7,6 +7,7 @@ import OpenEXR
 import cv2
 import bpy
 import numpy as np
+from blenderset.utils import mesh
 
 
 class ExrFile:
@@ -81,18 +82,8 @@ class ExrFile:
                             )
 
                     # 3D Bounding box
-                    bounds = []
-                    for o in list(bpy.data.objects[name].children_recursive) + [bpy.data.objects[name]]:
-                        if o.type == 'MESH':
-                            bound_box = o.bound_box
-                            bbox = np.column_stack([np.array(bound_box), np.ones(len(bound_box))]) @ np.array(o.matrix_world).T
-                            bounds.append(bbox)
-                    bounds = np.row_stack(bounds)
-                    x0, x1 = bounds[:,0].min(), bounds[:,0].max()
-                    y0, y1 = bounds[:,1].min(), bounds[:,1].max()
-                    z0, z1 = bounds[:,2].min(), bounds[:,2].max()
-                    bbox = [(x0, y0, z0), (x0, y0, z1), (x0, y1, z1), (x0, y1, z0), (x1, y0, z0), (x1, y0, z1), (x1, y1, z1), (x1, y1, z0)]
-                    obj["bounding_3d"] = bbox
+                    meshes = [o for o in list(bpy.data.objects[name].children_recursive) + [bpy.data.objects[name]] if o.type == 'MESH']
+                    obj["bounding_3d"] = mesh.bounding_box(meshes)
 
                     # SMPL Shape keys
                     for o in bpy.data.objects[name].children_recursive:
